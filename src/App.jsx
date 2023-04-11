7f087d2d329dc5e89fa425d7c7e6415e19dc7e5b
Raphael Lelis <raphael.lelis@take.net>
2023-04-11 15:57:48 -0300

    🥳 Initial project generator qrcode

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <input
+       placeholder='Digite seu link...'
+      />
+    </div>
+  )
+}
+
+export default App
