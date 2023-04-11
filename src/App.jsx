5314936fdb06e6423eea98e573c25ade229103d6
Raphael Lelis <raphael.lelis@take.net>
2023-04-11 16:31:17 -0300

    add motion e download image

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,50 @@
+import { useState } from 'react'
+import QRCode from 'react-qr-code'
+import QrCodeLink from 'qrcode'
+import { motion } from 'framer-motion'
 import './App.css'
 
 function App() {
+  const [link, setLink] = useState('')
+  const [qrcodeLink, setQrcodeLink] = useState('')
+
+  function handleGenerate(link_url){
+    QrCodeLink.toDataURL(link_url, {
+      width: 600,
+      margin: 3,
+    }, function (err, url){
+      setQrcodeLink(url);
+    })
+  }
+
+  function handleQrcode(e){
+    setLink(e.target.value);
+    handleGenerate(e.target.value);
+  }
 
   return (
-    <div className="App">
-      <input
-       placeholder='Digite seu link...'
+    <div className="container">
+      <motion.h1
+      initial={{ x: "100%" }}
+      animate={{ x: "0" }}
+      >Generate Qrcode</motion.h1>
+      <QRCode 
+        value={link}
       />
+
+      <input
+      className='input'
+       placeholder='Digite seu link...'
+       value={link}
+       onChange={ (e) => handleQrcode(e)}
+      />
+
+      <motion.a 
+      href={qrcodeLink} 
+      download={`qrcode.png`}
+      whileHover={{ scale: 1.5 }}
+      style={{ x: "0" }}
+      >Baixar QrCode</motion.a>
     </div>
   )
 }
